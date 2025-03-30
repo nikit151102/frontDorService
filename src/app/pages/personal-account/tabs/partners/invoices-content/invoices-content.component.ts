f207e4113f1e605fa3e2fb76aac615b0d68604e3
nikit151102 <nikit15110295@gmail.com>
2025-03-30 13:32:06 +0700

    fix field sumAmount

--- src/app/pages/personal-account/tabs/partners/invoices-content/invoices-content.component.ts
+++ src/app/pages/personal-account/tabs/partners/invoices-content/invoices-content.component.ts
@@ -53,7 +53,7 @@ export class InvoicesContentComponent implements OnInit, OnChanges {
     { field: 'quantity', fieldView: 'quantity', header: 'Количество', type: 'number', visible: true, width: '11%' },
     { field: 'measurementUnit.Id', fieldView: 'measurementUnit', filterType: 10, searchType: 'measurementUnit.Name', header: 'Ед.изм', type: 'uuid', visible: true, width: '11%', endpoint: '/api/Entities/MeasurementUnit/Filter' },
     // { field: 'amount', fieldView: 'amount', header: 'Сумма', type: 'number', visible: true, width: '11%' },
-    { field: 'sunAmount', fieldView: 'sunAmount', header: 'Общая сумма', type: 'number', visible: true, width: '11%' },
+    { field: 'sumAmount', fieldView: 'sumAmount', header: 'Общая сумма', type: 'number', visible: true, width: '11%' },
     { field: 'DocInvoice.Number', fieldView: 'docInvoice', header: 'Номер фактуры', type: 'string', visible: true, width: '10%', isFilter: false },
     { field: 'DocInvoice.DateTime', fieldView: 'dateTime', header: 'Дата фактуры', type: 'date', visible: true, width: '13%' },
     { field: 'DocInvoice.Status', fieldView: 'docInvoiceStatus', header: 'Статус фактуры', type: 'enam', visible: true, width: '16%' }
