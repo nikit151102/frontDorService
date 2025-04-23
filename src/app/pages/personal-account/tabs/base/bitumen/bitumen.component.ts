985d98a7cede37c279fc19289f0da167e723998c
Nikita <nikit15110295@gmail.com>
2025-04-24 01:57:36 +0700

    edit header

--- src/app/pages/personal-account/tabs/base/bitumen/bitumen.component.ts
+++ src/app/pages/personal-account/tabs/base/bitumen/bitumen.component.ts
@@ -38,7 +38,7 @@ export class BitumenComponent implements OnInit {
     { field: 'date', header: 'Дата', type: 'date', visible: true, width: '12%' },
     { field: 'auto', header: 'Авто', type: 'string', visible: true, width: '10%' },
     { field: 'placeFrom', header: 'Откуда', type: 'string', visible: true, width: '10%' },
-    { field: 'placeTo', header: 'Куда слил', type: 'string', visible: true, width: '10%' },
+    { field: 'placeTo', header: 'Куда слилb', type: 'string', visible: true, width: '10%' },
     {
       field: 'cargoName',
       fieldView: 'cargoName',
