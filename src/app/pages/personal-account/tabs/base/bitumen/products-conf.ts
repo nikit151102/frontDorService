5b1f58e5d5d10a3512f136168f40c17488d9a72e
Nikita <nikit15110295@gmail.com>
2025-04-24 23:26:57 +0700

    fix fields

--- src/app/pages/personal-account/tabs/base/bitumen/products-conf.ts
+++ src/app/pages/personal-account/tabs/base/bitumen/products-conf.ts
@@ -11,8 +11,8 @@ export const CONFIGPRODUCTS: any = [
             { field: 'Cargo.Id', fieldView: 'cargoName', filterType: 10, searchField: 'Cargo.Name', header: 'Груз', type: 'uuid', visible: true, width: '16%', endpoint: '/api/Entities/Cargo/Filter' },
             { field: 'Weight', fieldView: 'weight', header: 'Тоннаж', type: 'string', visible: true, width: '18%' },
             { field: 'Amount', fieldView: 'amount', header: 'Цена', type: 'number', visible: true, width: '18%' },
-            { field: 'SumAmount', fieldView: 'SumAmount', header: 'Сумма', type: 'number', visible: true, width: '18%' },
-            { field: 'PaymentType', fieldView: 'paymentType', header: 'Цена', type: 'number', visible: true, width: '18%' },
+            { field: 'SumAmount', fieldView: 'sumAmount', header: 'Сумма', type: 'number', visible: true, width: '18%' },
+            { field: 'PaymentType', fieldView: 'paymentType', header: 'Форма оплаты', type: 'number', visible: true, width: '18%' },
 
         ],
         totalInfoColumn: [
@@ -31,8 +31,8 @@ export const CONFIGPRODUCTS: any = [
             { field: 'Cargo.Id', fieldView: 'cargoName', filterType: 10, searchField: 'Cargo.Name', header: 'Груз', type: 'uuid', visible: true, width: '16%', endpoint: '/api/Entities/Cargo/Filter' },
             { field: 'Weight', fieldView: 'weight', header: 'Тоннаж', type: 'string', visible: true, width: '18%' },
             { field: 'Amount', fieldView: 'amount', header: 'Цена', type: 'number', visible: true, width: '18%' },
-            { field: 'SumAmount', fieldView: 'SumAmount', header: 'Сумма', type: 'number', visible: true, width: '18%' },
-            { field: 'PaymentType', fieldView: 'paymentType', header: 'Цена', type: 'number', visible: true, width: '18%' },
+            { field: 'SumAmount', fieldView: 'sumAmount', header: 'Сумма', type: 'number', visible: true, width: '18%' },
+            { field: 'PaymentType', fieldView: 'paymentType', header: 'Форма оплаты', type: 'number', visible: true, width: '18%' },
         ],
         totalInfoColumn: [
             { columnNum: 3, value: 'TotalSums' },
