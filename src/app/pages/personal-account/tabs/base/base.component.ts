2bebcde0448dcaea9d8c57123545eeb86bc6ae48
Nikita <nikit15110295@gmail.com>
2025-04-25 22:33:31 +0700

    вебсокеты во вкладке база

--- src/app/pages/personal-account/tabs/base/base.component.ts
+++ src/app/pages/personal-account/tabs/base/base.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BaseMenuComponent } from './base-menu/base-menu.component';
 
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { BaseService } from './base.service';
 
 @Component({
   selector: 'app-base',
@@ -10,19 +11,23 @@ import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
   templateUrl: './base.component.html',
   styleUrl: './base.component.scss'
 })
-export class BaseComponent {
+export class BaseComponent implements OnInit{
 
   selectedTab: any;
   selectedTabConfig: any;
 
-  constructor(private router: Router, private activatedRoute:ActivatedRoute) { }
+  constructor(private router: Router, private activatedRoute:ActivatedRoute, private baseService:BaseService) { }
+  
+  ngOnInit(): void {
+    this.baseService.connectToWebSocket();
+  }
 
   onSelectTab(selectTab: any) {
     console.log('selectTab', selectTab);
   
     this.selectedTab = selectTab.code;
     sessionStorage.setItem('managerDocType', selectTab.managerDocType);
-  
+
     let navigatePath;
   
     if (typeof selectTab.navigate === 'function') {
--- src/app/pages/personal-account/tabs/base/base.service.ts
+++ src/app/pages/personal-account/tabs/base/base.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { InvoicesService } from '../../components/invoices/invoices.service';
+import { environment } from '../../../../../environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
 
-  constructor() { }
+  constructor(public invoicesService: InvoicesService) { }
+
+  private socket!: WebSocket;
+
+  selectManagerDocType: any;
+
+  connectToWebSocket(): void {
+    const token = localStorage.getItem('YXV0aFRva2Vu');
+    let apiUrl = environment.apiUrl.replace(/^https/, "wss");
+    const url = `${apiUrl}/auth/WebsocketConnect?token=${token}&queueTag=managerDocTable`;
+    this.socket = new WebSocket(url);
+
+    this.socket.onopen = () => {
+    };
+
+    this.socket.onmessage = (event) => {
+      try {
+        const data = JSON.parse(event.data);
+        console.log("web-socket works:", data);
+        console.log('data.managerDocType', data.managerDocType);
+        console.log('this.selectManagerDocType', this.selectManagerDocType)
+        if (this.selectManagerDocType === data.managerDocType) {
+          this.invoicesService.addOrUpdateItem(data);
+        }
+      } catch (e) {
+        console.error('Error parsing WebSocket message:', e);
+      }
+    };
+
+    this.socket.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+
+    this.socket.onclose = () => {
+    };
+  }
+
+  disconnectWebSocket(): void {
+    if (this.socket) {
+      this.socket.close();
+    }
+  }
 }
+
