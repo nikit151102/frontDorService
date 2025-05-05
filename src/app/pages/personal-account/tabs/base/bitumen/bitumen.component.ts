39f04df31b3e396e7f6831fb0fdaf8ee09c09626
Nikita <nikit15110295@gmail.com>
2025-05-05 10:27:40 +0700

    bitum edit field placeTo

--- src/app/pages/personal-account/tabs/base/bitumen/bitumen.component.ts
+++ src/app/pages/personal-account/tabs/base/bitumen/bitumen.component.ts
@@ -69,7 +69,7 @@ export class BitumenComponent implements OnInit {
   columnsExpenseData = [
     { field: 'date', header: 'Дата', type: 'date', visible: true, width: '12%' },
     { field: 'auto', header: 'Авто', type: 'string', visible: true, width: '10%' },
-    { field: 'placeFrom', header: 'Откуда', type: 'string', visible: true, width: '10%' },
+    { field: 'placeTo', header: 'Откуда', type: 'string', visible: true, width: '10%' },
     { field: 'organization', header: 'Кому', type: 'string', visible: true, width: '10%' },
     {
       field: 'cargoName',
