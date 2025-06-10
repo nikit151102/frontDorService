c534bb3c5cc23043cd30ec4fb2087621c212d2df
Nikita <nikit15110295@gmail.com>
2025-06-10 21:20:27 +0700

    EDIT. скрыто field: 'status'

--- src/app/pages/personal-account/tabs/logistic/general-docs/config.ts
+++ src/app/pages/personal-account/tabs/logistic/general-docs/config.ts
@@ -9,7 +9,7 @@ export const totalInfoColumn = [
 
 export const columnsDocs = [
     { field: 'productTarget.Id', fieldView: 'productTarget', filterType: 10, searchField: 'productTarget.Name', header: 'Назначение', type: 'uuid', visible: true, width: '16%', endpoint: '/api/Entities/ProductTarget/Filter' },
-    { field: 'status', header: 'Статус', type: 'enum', visible: true, width: '15%' },
+    // { field: 'status', header: 'Статус', type: 'enum', visible: true, width: '15%' },
     { field: 'beginDateTime', header: 'Начало периода', type: 'date', visible: true, width: '15%' },
     { field: 'endDateTime', header: 'Окончание периода', type: 'date', visible: true, width: '15%' },
     { field: 'differentOdometer', header: 'Разница (км)', type: 'number', visible: true, width: '15%' },
