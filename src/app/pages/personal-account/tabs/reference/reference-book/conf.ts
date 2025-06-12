f268167a32b33ac33cd1a2e2ee094975ec63c2cb
Nikita <nikit15110295@gmail.com>
2025-06-13 00:50:54 +0700

    FIX field

--- src/app/pages/personal-account/tabs/reference/reference-book/conf.ts
+++ src/app/pages/personal-account/tabs/reference/reference-book/conf.ts
@@ -120,7 +120,7 @@ export const referenceConfig = [
 
         ],
         formFields: [
-            { label: 'Фамилия', field: 'surname', type: 'type', visible: true  },
+            { label: 'Фамилия', field: 'surname', type: 'text', visible: true  },
             { label: 'Имя', field: 'name', type: 'text', visible: true },
             { label: 'Отчество', field: 'patronymic', type: 'text', visible: true },
         ],
