f79dc5ff06818d0a4116e060367be55c7090f045
nikita <nikit15110295@gmail.com>
2026-07-20 16:27:38 +0700

    fix

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     // apiUrl: 'https://localhost:5711',
     //apiUrl: 'https://localhost:5001',
-    // apiUrl: 'https://hanagyan.ru/api2',
-    apiUrl: 'https://dorservice.actual:5711',
+    apiUrl: 'https://hanagyan.ru/api2',
+    // apiUrl: 'https://dorservice.actual:5711',
     // apiUrl: 'https://vm-a4fdd9dc.na4u.ru:5711',
     token: 'YXV0aFRva2Vu',
     userAuthData: '1c0b8f3d4e2a9f8c7b6a5d4e3f2a1b0c9d8e7f6a5b4c3d2e1f0a9b8c7d6e5f4'
